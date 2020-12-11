fd736640a228153b87403c96620612d92649b054
Michael Perez <mperez6473@gmail.com>
2020-12-11 15:02:25 -0600

    More learning through django. Using a virtual env this time around.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'polls'
+# pretty much created a route
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Question
 
 
+# def index(request):
+#     return HttpResponse('Hello, world. You\'re at the polls index.')
+
+
+# Get questions and display them
 def index(request):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_quesiton_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
